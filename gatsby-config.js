f851c018d22d5de793b35149a4fc6d2d2ffb57e5
Khranovskiy <xronosd@gmail.com>
2019-03-04 02:20:46 +0300

    update README: remove autogenerated text

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Khranovsky Blog`,
+    title: `Khranovsky's Blog`,
     author: `Denis Khranovsky`,
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://khranovsky.com`,
