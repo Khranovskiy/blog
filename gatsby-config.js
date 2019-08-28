781ae2f60e60823b47f81ee35950106b3fcd5ac6
Khranovskiy <xronosd@gmail.com>
2019-08-28 04:18:36 +0300

    temporary netlify cms plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,6 +67,12 @@ module.exports = {
       },
     },
     `gatsby-plugin-offline`,
+    {
+      resolve: "gatsby-plugin-netlify-cms"
+      // options: {
+      //   modulePath: `${__dirname}/src/cms/cms.js`,
+      // },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-typography`,
