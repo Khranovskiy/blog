556d40e425bc5f282ce46a68575bec8e2648804c
Khranovskiy <xronosd@gmail.com>
2019-09-09 10:12:01 +0300

    gatsbyjs/gatsby-starter-blog-theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,44 +1,24 @@
 module.exports = {
-  siteMetadata: {
-    title: `Novela by Narative`,
-    name: `Narative`,
-    siteUrl: `https://novela.narative.co`,
-    description: `This is my description that will be used in the meta tags and important for search results`,
-    hero: {
-      heading: `Welcome to Novela, the simplest way to start publishing with Gatsby.`,
-      maxWidth: 652,
+  plugins: [
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {},
     },
+  ],
+  // Customize your site metadata:
+  siteMetadata: {
+    title: `My Blog Title`,
+    author: `My Name`,
+    description: `My site description...`,
     social: [
       {
         name: `twitter`,
-        url: `https://twitter.com/narative`,
+        url: `https://twitter.com/gatsbyjs`,
       },
       {
         name: `github`,
-        url: `https://github.com/narative`,
-      },
-      {
-        name: `instagram`,
-        url: `https://instagram.com/narative.co`,
-      },
-      {
-        name: `linkedin`,
-        url: `https://www.linkedin.com/company/narative/`,
-      },
-      {
-        name: `dribbble`,
-        url: `https://dribbble.com/narativestudio`,
+        url: `https://github.com/gatsbyjs`,
       },
     ],
   },
-  plugins: [
-    {
-      resolve: "@narative/gatsby-theme-novela",
-      options: {
-        contentPosts: "content/posts",
-        contentAuthors: "content/authors",
-        basePath: "/",
-      },
-    },
-  ],
-};
+}
