7719324c062b5343ab86c899f9805537a944e1ad
Khranovskiy <xronosd@gmail.com>
2019-09-09 11:59:47 +0300

    use theme @narative/gatsby-theme-novela

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,17 +4,38 @@ module.exports = {
     author: `Denis Khranovsky`,
     description: `Denis Khranovsky Blog`,
     siteUrl: `https://khranovsky.com`,
+    name: 'Khranovsky',
+    hero: {
+      heading: `Welcome to Novela, the simplest way to start publishing with Gatsby.`,
+      maxWidth: 652,
+    },
     social: [
       {
         name: `twitter`,
-        url: `https://twitter.com/xronosd`
-      }
-    ]
+        url: `https://twitter.com/xronosd`,
+      },
+      {
+        name: `github`,
+        url: `http://github.com/khranovskiy/`,
+      },
+      {
+        name: `instagram`,
+        url: `https://www.instagram.com/xronosd/`,
+      },
+      {
+        name: `linkedin`,
+        url: `http://linkedin.com/in/denis-khranovskiy/`,
+      },
+    ],
   },
   plugins: [
     {
-      resolve: `gatsby-theme-blog`,
-      options: {}
+      resolve: "@narative/gatsby-theme-novela",
+      options: {
+        contentPosts: "content/posts",
+        contentAuthors: "content/authors",
+        basePath: "/",
+      },
     },
     {
       resolve: `gatsby-plugin-netlify-cms`
